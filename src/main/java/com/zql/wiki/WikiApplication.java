940f2d16c0dc12ec095d1a454961165ded55549c
Reshape <1742809983@qq.com>
2024-10-11 12:08:20 +0800

    2-5 优化启动

--- src/main/java/com/zql/wiki/WikiApplication.java
+++ src/main/java/com/zql/wiki/WikiApplication.java
@@ -1,12 +1,18 @@
 package com.zql.wiki;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.slf4j.Logger;
 
 @SpringBootApplication
 public class WikiApplication {
-
+    private static final Logger LOG= LoggerFactory.getLogger(WikiApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app=new SpringApplication(WikiApplication.class);
+        Environment env=app.run(args).getEnvironment();
+        LOG.info("启动成功!!");
+        LOG.info("地址: http://127.0.0.1:{}",env.getProperty("server.port"));
     }
 }
