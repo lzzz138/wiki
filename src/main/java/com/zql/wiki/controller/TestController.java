cdd4db4b2796daac2e5b04215d6f47cbd27d7782
Reshape <1742809983@qq.com>
2024-10-12 15:51:34 +0800

    2-8 自定义配置文件

--- src/main/java/com/zql/wiki/controller/TestController.java
+++ src/main/java/com/zql/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.zql.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 //返回字符串
 @RestController
 public class TestController {
+    @Value("${test.hello:Test}")
+    private String hello;
+
     //接口地址
     @RequestMapping("/hello")
     public String hello() {
-        return "hello world";
+        return "hello world"+hello;
     }
 
     @PostMapping("/hello/post")
