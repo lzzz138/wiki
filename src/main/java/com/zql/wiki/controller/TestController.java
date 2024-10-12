7e548fa5301832bdd02919ca4c192618377cffa2
Reshape <1742809983@qq.com>
2024-10-12 21:24:54 +0800

    3-4 最外层入口，进入TestService

--- src/main/java/com/zql/wiki/controller/TestController.java
+++ src/main/java/com/zql/wiki/controller/TestController.java
@@ -1,15 +1,24 @@
 package com.zql.wiki.controller;
 
+import com.zql.wiki.domain.Test;
+import com.zql.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 //返回字符串
 @RestController
 public class TestController {
     @Value("${test.hello:Test}")
     private String hello;
+
+    @Resource
+    private TestService testService;
 
     //接口地址
     @RequestMapping("/hello")
@@ -20,5 +29,10 @@ public class TestController {
     @PostMapping("/hello/post")
      public String helloPost(String name) {
         return "hello " + name;
+    }
+
+    @GetMapping("/hello/list")
+    public List<Test> list(){
+        return testService.list();
     }
 }
