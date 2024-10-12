24c3faf48f4f05dd5ebfcd6f39f6e001c2bf58fa
Reshape <1742809983@qq.com>
2024-10-12 21:27:30 +0800

    3-4 添加MapperScan扫描接口层

--- src/main/java/com/zql/wiki/WikiApplication.java
+++ src/main/java/com/zql/wiki/WikiApplication.java
@@ -1,12 +1,14 @@
 package com.zql.wiki;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
-import org.slf4j.Logger;
 
 @SpringBootApplication
+@MapperScan("com.zql.wiki.mapper")
 public class WikiApplication {
     private static final Logger LOG= LoggerFactory.getLogger(WikiApplication.class);
     public static void main(String[] args) {
