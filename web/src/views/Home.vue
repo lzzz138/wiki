2c4fed88fe8a42fddec2e0f12c9cc55132a9a876
Reshape <1742809983@qq.com>
2024-10-22 16:37:50 +0800

    5-5 axios修改请求域为配置文件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -98,8 +98,8 @@ export default {
     const ebooks=ref();
 
     onMounted(()=>{
-      console.log("onMounted");
-      axios.get("http://localhost:8880/ebook/list?").then((response)=>{
+      console.log("onMounted111");
+      axios.get(process.env.VUE_APP_SERVER+"/ebook/list?").then((response)=>{
         const data=response.data;
         ebooks.value=data.content;
         console.log(response);
