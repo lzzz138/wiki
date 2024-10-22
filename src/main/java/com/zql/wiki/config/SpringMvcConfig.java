db0cee228840bb286f37dbc9ff3ae24a04050508
Reshape <1742809983@qq.com>
2024-10-22 20:15:00 +0800

    5-9 新增aop打印日志，删除拦截器过滤器

--- src/main/java/com/zql/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/zql/wiki/config/SpringMvcConfig.java
@@ -1,25 +1,25 @@
-package com.zql.wiki.config;
-
-import com.zql.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor logInterceptor;
-
-    /**
-     * 将日志拦截器注入
-     * @param registry
-     */
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                //针对所有的请求
-                .addPathPatterns("/**").excludePathPatterns("/login");
-    }
-}
+//package com.zql.wiki.config;
+//
+//import com.zql.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//    /**
+//     * 将日志拦截器注入
+//     * @param registry
+//     */
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                //针对所有的请求
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//    }
+//}
