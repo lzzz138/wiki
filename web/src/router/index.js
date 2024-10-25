0bee82bc76ed9aea8acf45e8a58b5ccc0445de05
Reshape <1742809983@qq.com>
2024-10-25 16:09:01 +0800

    6-2 新增管理页面并增添路由

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/home.vue'
-import About from "@/views/about.vue";
+import About from "../views/about.vue";
+import AdminEbook from "../views/admin/admin-ebook.vue";
 
 const routes = [
   {
@@ -16,6 +17,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     // component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
+  },
+  {
+    path: '/admin/ebook',
+    name: 'AdminEbook',
+    component: AdminEbook
   }
 ]
 
