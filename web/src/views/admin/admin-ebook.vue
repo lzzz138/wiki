15f74240443b28bb703ec4d9d5be3c5cf2255133
Reshape <1742809983@qq.com>
2025-09-05 17:15:16 +0800

    7-7 修改查询异步bug

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -318,10 +318,7 @@
 
       onMounted(()=>{
         handleQueryCategory();
-        handleQuery({
-          page : 1,
-          size : pagination.value.pageSize,
-        });
+
       });
 
       return{
