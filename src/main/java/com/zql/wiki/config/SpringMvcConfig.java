b998937f7b83437be9ab00fccea184e89b392f04
Reshape <1742809983@qq.com>
2025-09-26 19:36:30 +0800

    9-11 后端增加登录校验，前端发生请求时头部带token，通过拦截器检验用户身份从而限制只有登录才能使用的功能

--- src/main/java/com/zql/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/zql/wiki/config/SpringMvcConfig.java
@@ -1,25 +1,32 @@
-//package com.zql.wiki.config;
-//
-//import com.zql.wiki.interceptor.LogInterceptor;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import javax.annotation.Resource;
-//
-//@Configuration
-//public class SpringMvcConfig implements WebMvcConfigurer {
-//
-//    @Resource
-//    LogInterceptor logInterceptor;
-//
-//    /**
-//     * 将日志拦截器注入
-//     * @param registry
-//     */
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(logInterceptor)
-//                //针对所有的请求
-//                .addPathPatterns("/**").excludePathPatterns("/login");
-//    }
-//}
+package com.zql.wiki.config;
+
+import com.zql.wiki.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/vote/**",
+                        "/doc/find-content/**",
+                        "/ebook-snapshot/**"
+                );
+    }
+}
